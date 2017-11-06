0acf851a502604897271e24002994e1267e149f1
Diana Kleban <dik548@g.harvard.edu>
2017-11-06 12:35:34 -0600

    Added migrations

---
+++ database/migrations/2017_11_06_030115_create_courses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+
+            $table->string('title');
+		        $table->string('desc');
+		        $table->integer('price')->comment('Hourly rate per course');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
